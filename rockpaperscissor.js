42e4714b8a92af100d5975d5a8c3802420d403c9
“nailref” <ferlianmunil110298@gmail.com>
2024-10-11 23:30:24 +0200

    add humanChoice code

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -11,3 +11,21 @@ function getComputerChoice() {
      return getComputerChoice();
     }
 };
+function getHumanChoice() {
+    let humanChoice = prompt("Choose between rock, paper or scissors");
+  
+      if (humanChoice) {
+      if (humanChoice === "rock") {
+        return "Rock";
+      } else if (humanChoice === "paper" ) {
+        return "Paper";
+      } else if (humanChoice === "scissors") {
+        return "Scissors";
+      } else {
+        alert("Invalid input. Choose between rock, paper or scissors");
+        return getHumanChoice();
+      }
+    } else {
+      alert("You canceled the game.");
+    }
+  }
